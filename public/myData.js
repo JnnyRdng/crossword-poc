1f785b95e56a78aea26eb8fe2ae8e5ed9bffb667
Jonny Reading <jonnyreading@gmail.com>
2021-01-19 19:17:07 +0000

    change data structure, put cell index in questions array

--- public/myData.js
+++ public/myData.js
@@ -3,33 +3,29 @@
     "dimensions": { "width": 13, "height": 13 },
     "board": ".degenerate...e.l.i.e.o.h.cleancut.trot.f.c.k.r.a.b..twig.telling...a.c.a...o..ruleofthumb..e...s.s.p...pastime.edit..s.r.e.s.a.h.soda.tipsters.n.i.i.e.e.o...blackwidow."
   },
-  {
-    "across": [
-      { "num": 1, "question": "Dissolute" },
-      { "num": 7, "question": "Wholesome – well-groomed" },
-      { "num": 8, "question": "Run briskly" },
-      { "num": 9, "question": "Catch on belatedly (informal)" },
-      { "num": 10, "question": "Significant" },
-      { "num": 12, "question": "Pragmatic guideline" },
-      { "num": 14, "question": "Fun activity" },
-      { "num": 16, "question": "Rewrite" },
-      { "num": 19, "question": "Carbonated drink" },
-      { "num": 20, "question": "Punters may pay for their advice" },
-      { "num": 21, "question": "Poisonous New World spider" }
-    ],
-    "down": [
-      { "num": 1, "question": "Glazed white and blue earthenware" },
-      { "num": 2, "question": "Lacking any warmth or friendliness" },
-      { "num": 3, "question": "Small cut" },
-      { "num": 4, "question": "Goes back" },
-      { "num": 5, "question": "Completely wreck – the lot" },
-      { "num": 6, "question": "Socialise (with)" },
-      { "num": 11, "question": "Superficial" },
-      { "num": 12, "question": "Argue" },
-      { "num": 13, "question": "Modernised" },
-      { "num": 15, "question": "Follow – path" },
-      { "num": 17, "question": "Light cloth covering for furniture" },
-      { "num": 18, "question": "Vomit" }
-    ]
-  }
+  [
+    { "index": 1, "direction": "across", "num": 1, "question": "Dissolute" },
+    { "index": 26, "direction": "across", "num": 7, "question": "Wholesome – well-groomed" },
+    { "index": 35, "direction": "across", "num": 8, "question": "Run briskly" },
+    { "index": 53, "direction": "across", "num": 9, "question": "Catch on belatedly (informal)" },
+    { "index": 58, "direction": "across", "num": 10, "question": "Significant" },
+    { "index": 79, "direction": "across", "num": 12, "question": "Pragmatic guideline" },
+    { "index": 104, "direction": "across", "num": 14, "question": "Fun activity" },
+    { "index": 112, "direction": "across", "num": 16, "question": "Rewrite" },
+    { "index": 130, "direction": "across", "num": 19, "question": "Carbonated drink" },
+    { "index": 135, "direction": "across", "num": 20, "question": "Punters may pay for their advice" },
+    { "index": 158, "direction": "across", "num": 21, "question": "Poisonous New World spider" },
+    { "index": 1, "direction": "down", "num": 1, "question": "Glazed white and blue earthenware" },
+    { "index": 3, "direction": "down", "num": 2, "question": "Lacking any warmth or friendliness" },
+    { "index": 5, "direction": "down", "num": 3, "question": "Small cut" },
+    { "index": 7, "direction": "down", "num": 4, "question": "Goes back" },
+    { "index": 9, "direction": "down", "num": 5, "question": "Completely wreck – the lot" },
+    { "index": 24, "direction": "down", "num": 6, "question": "Socialise (with)" },
+    { "index": 70, "direction": "down", "num": 11, "question": "Superficial" },
+    { "index": 79, "direction": "down", "num": 12, "question": "Argue" },
+    { "index": 87, "direction": "down", "num": 13, "question": "Modernised" },
+    { "index": 107, "direction": "down", "num": 15, "question": "Follow – path" },
+    { "index": 115, "direction": "down", "num": 17, "question": "Light cloth covering for furniture" },
+    { "index": 124, "direction": "down", "num": 18, "question": "Vomit" }
+  ]
 ]
