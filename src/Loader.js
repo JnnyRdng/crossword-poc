96fc2e8617c40c6a101b57884dc3ea7768b332ef
Jonny Reading <jonnyreading@gmail.com>
2021-01-19 14:41:18 +0000

    add loading as a component

--- src/Loader.js
+++ src/Loader.js
@@ -1,24 +1,26 @@
 import { useState, useEffect } from "react";
 import App from "./App";
+import Loading from "./Components/Loading";
 
 export default function Loader() {
   const [config, setConfig] = useState(undefined);
   const [questions, setQuestions] = useState(undefined);
 
   useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = () => {
     fetch("/crossword/myData.js")
       .then(res => res.json())
       .then(res => {
         setConfig(res[0]);
         setQuestions(res[1]);
       });
-
-  }, []);
+  }
 
 
   return (config && questions) ?
     <App config={config} questions={questions} /> :
-    (
-      <p>Loading...</p>
-    );
+    <Loading />;
 }
